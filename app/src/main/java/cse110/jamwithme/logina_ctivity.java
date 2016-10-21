faca6d44a0e599f342b9b0ccb6ff470acc8d729c
Paula Q <pnquach@ucsd.edu>
2016-10-21 00:47:54 -0700

    Adding "User" class to build off of later

--- app/src/main/java/cse110/jamwithme/logina_ctivity.java
+++ app/src/main/java/cse110/jamwithme/logina_ctivity.java
@@ -22,6 +22,7 @@ public class logina_ctivity extends AppCompatActivity {
     private EditText Login_name;
     private EditText Login_Pass;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +38,7 @@ public class logina_ctivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if(firebaseAuth.getCurrentUser() != null){
                     finish();
-                    startActivity(new Intent(logina_ctivity.this,UserProfileActivity.class));
+                    startActivity(new Intent(logina_ctivity.this, UserProfileActivity.class));
                 }
             }
         };
