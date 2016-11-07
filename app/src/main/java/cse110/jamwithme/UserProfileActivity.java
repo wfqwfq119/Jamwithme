1a93627800c0d8d3f173bb9986ecced608ec4029
njponce <njponce72@gmail.com>
2016-11-06 17:58:36 -0800

    camera and layout 2

--- app/src/main/java/cse110/jamwithme/UserProfileActivity.java
+++ app/src/main/java/cse110/jamwithme/UserProfileActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     public Button add_jams;
+    private ImageView ivProfile;
+    private ImageButton camButton;
 
     // Give 'add_jams' button functionality
     public void init() {
@@ -23,11 +27,26 @@ public class UserProfileActivity extends AppCompatActivity {
         });
     }
 
+    /** GIVE CAMERA BUTTON FUNCTIONALITY **/
+    public void cameraButton() {
+        ivProfile = (ImageView) findViewById(R.id.ivProfile);
+        camButton = (ImageButton) findViewById(R.id.iBTakePic);
+        camButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent image = new Intent(UserProfileActivity.this,
+                        camera.class);
+                startActivity(image);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         init();
+        cameraButton();
     }
 }
 
